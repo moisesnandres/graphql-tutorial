f7eb45429c591e6528830d8ca42801fd7f28ef8f
Moisés Ñañez <moisesnandres@gmail.com>
2017-07-05 13:00:52 -0500

    Set up GraphiQL

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,17 +1,16 @@
 import {
   makeExecutableSchema,
-  addMockFunctionsToSchema, # we'll use this later
+  addMockFunctionsToSchema,
 } from 'graphql-tools';
 
 const typeDefs = `
 type Channel {
-   id: ID!                # "!" denotes a required field
+   id: ID!
    name: String
 }
-# This type specifies the entry points into our API. In this case
-# there is only one - "channels" - which returns a list of channels.
+
 type Query {
-   channels: [Channel]    # "[]" means this is a list of channels
+   channels: [Channel]
 }
 `;
 const schema = makeExecutableSchema({ typeDefs });
