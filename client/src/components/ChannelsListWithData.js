abcbf6f70b1d277b025b0cd471738bfbaf6815f4
Moisés Ñañez <moisesnandres@gmail.com>
2017-07-06 13:00:52 -0500

    Add components for channel view

--- client/src/components/ChannelsListWithData.js
+++ client/src/components/ChannelsListWithData.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import {
+  Link
+} from 'react-router-dom'
+
+import {
     gql,
     graphql,
 } from 'react-apollo';
-import AddChannel from './AddChannel'
+
+import AddChannel from './AddChannel';
 
 const ChannelsList = ({ data: {loading, error, channels }}) => {
   if (loading) {
@@ -16,7 +21,13 @@ const ChannelsList = ({ data: {loading, error, channels }}) => {
   return (
     <div className="channelsList">
       <AddChannel />
-      { channels.map( ch => <div key={ch.id} className={'channel ' + (ch.id < 0 ? 'optimistic' : '')}>{ch.name}</div> ) }
+      { channels.map( ch =>
+        (<div key={ch.id} className={'channel ' + (ch.id < 0 ? 'optimistic' : '')}>
+          <Link to={ch.id < 0 ? `/` : `channel/${ch.id}`}>
+            {ch.name}
+          </Link>
+        </div>)
+      )}
     </div>
   );
 };
